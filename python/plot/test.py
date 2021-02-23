d3c519eb83b5a31378fd5cd9169fbce8a15f0b53
Steve Lerner <stevelerner@gmail.com>
2021-02-23 15:03:38 -0500

    Update test.py

--- python/plot/test.py
+++ python/plot/test.py
@@ -1,3 +1,3 @@
 from slack_webhook import Slack
-slack = Slack(url='https://hooks.slack.com/services/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')
+slack = Slack(url='https://hooks.slack.com/services/T010034DFV3/B01NV387HDL/SECRET')
 slack.post(text="Hello, world.")
