46672172d848aef59f1f55a8a54c781aed9e5f52
Steve Lerner <stevelerner@gmail.com>
2021-04-25 12:59:42 -0400

    commit

--- autogen/buildcontainer-autogen.sh
+++ autogen/buildcontainer-autogen.sh
@@ -1,4 +1,4 @@
 # requires dockerhub login
 sudo docker build . -f dockerfile-python-autogen -t python-autogen && \
-sudo docker tag splk-python-autogen stevelerner/python-autogen && \
+sudo docker tag python-autogen stevelerner/python-autogen && \
 sudo docker push stevelerner/python-autogen
