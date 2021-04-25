8244a5fd198c61a857ef450bd965aeffd263810f
Steve Lerner <stevelerner@gmail.com>
2021-04-25 19:53:56 -0400

    commit

--- apm-wip/docker/buildcontainer-print.sh
+++ apm-wip/docker/buildcontainer-print.sh
@@ -1,4 +1,4 @@
 # requires dockerhub login
-sudo docker build . -f dockerfile-python-print -t python-print && \
+sudo docker build . -f dockerfile-python-print -t python-print
 # sudo docker tag python-print stevelerner/python-print && \
 # sudo docker push stevelerner/python-print
